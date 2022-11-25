bb280fd698a65ebb2041d2f92022d978d60eedc6
20121974 <nfi46102@mail.ru>
2022-11-25 12:22:02 +0300

    1 commit

---
+++ src/main/java/com/skypro/string/cart/CartServisApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.string.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartServisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartServisApplication.class, args);
+    }
+
+}
